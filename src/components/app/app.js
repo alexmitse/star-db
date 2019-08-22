5b7c07711be235df32b9002f7d9315e81cff56a8
alexmitse <alex1998mitsevich@gmail.com>
2019-08-22 16:21:04 +0300

    not end DidCatch realisation

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 
 import Header from '../header';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 import PlanetDetails from '../planet-details';
 import RandomPlanet from '../random-planet';
 import StarshipDetails from '../starship-details';
+import ErrorIndicator from '../errorIndicator';
+import PeoplePage from '../people-page';
 
 
 import './app.css';
@@ -14,29 +14,46 @@ export default class App extends Component {
 
     state = {
         showRandomPlanet: true,
-        selectedPerson: null
+        hasError: false
     };
 
-    onPersonSelected = (id) => {
-        this.setState({
-            selectedPerson: id
+    toggleRandomPlanet = () => {
+        this.setState((state) => {
+        return {
+            showRandomPlanet: !state.showRandomPlanet
+        }
         });
     };
 
+
+    componentDidCatch() {
+        console.log('Did Catch');
+        this.setState({hasError: true});
+    }
+
     render() {
+
+        if (this.state.hasError) {
+            return <ErrorIndicator />
+        }
+
+        const planet = this.state.showRandomPlanet ?
+            <RandomPlanet/> : 
+            null;
+
         return (
             <div>
                 <Header />
-                <RandomPlanet />
+                { planet }
 
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList OnItemSelected={this.onPersonSelected} />
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId = {this.state.selectedPerson} />
-                    </div>
-                </div>
+                <button
+                    className="toggle-planet btn btn-warning btn-lg"
+                    onClick={this.toggleRandomPlanet}>
+                    Toggle Random Planet
+                </button>
+
+                <PeoplePage />
+
             </div>
         )
     }
