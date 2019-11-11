f0cc8cbe52b49142242327d73bfd0ed9a9fccf79
alexmitse <alex1998mitsevich@gmail.com>
2019-11-11 15:31:28 +0300

    feat(routing): add query params for pesone

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -15,12 +15,8 @@ export default function Pagination({
 }) {
   const [pageSize] = useState(10);
   const [totalCounts] = useState(totalCount);
-  let [currentPages] = useState(currentPage);
-  let [prevPage] = useState(0);
-
-  function onRenderPageNumber(item) {
-    currentPages = item;
-  }
+  const [currentPages, setCurrentPages] = useState(currentPage);
+  const [prevPage, setPrevPage] = useState(0);
 
   function useQuery() {
     return new URLSearchParams(useLocation().search);
@@ -28,6 +24,7 @@ export default function Pagination({
 
   const query = useQuery();
   const pagesCount = Math.ceil(totalCounts / pageSize);
+
   const pages = [];
   for (let i = 1; i <= pagesCount; i += 1) {
     pages.push(i);
@@ -35,8 +32,8 @@ export default function Pagination({
 
   if (prevPage !== query.get('page')) {
     onSelectNumber(query.get('page'));
-    onRenderPageNumber(query.get('page'));
-    prevPage = query.get('page');
+    setCurrentPages(query.get('page'));
+    setPrevPage(query.get('page'));
   }
 
   return (
@@ -44,17 +41,17 @@ export default function Pagination({
       <ul className="pagination">
         <Link
           to={`/people?page=${
-            currentPages === pages[0] ? currentPages - 1 : currentPages
+            +currentPages === pages[0] ? +currentPage : +currentPage - 1
           }`}
         >
           <li
             className="page-item-select"
             onClick={() => {
               onSelectNumber(
-                currentPages === pages[0] ? currentPages : currentPages - 1,
+                +currentPages === pages[0] ? +currentPages : +currentPages - 1,
               );
-              onRenderPageNumber(
-                currentPages === pages[0] ? currentPages : currentPages - 1,
+              setCurrentPages(
+                +currentPages === pages[0] ? +currentPages : +currentPages - 1,
               );
             }}
           >
@@ -64,11 +61,11 @@ export default function Pagination({
         <Link to={`/people?page=${pages[0]}`}>
           <li
             className={
-              currentPages === pages[0] ? 'page-item-selected' : 'page-item'
+              +currentPages === pages[0] ? 'page-item-selected' : 'page-item'
             }
             onClick={() => {
               onSelectNumber(pages[0]);
-              onRenderPageNumber(pages[0]);
+              setCurrentPages(pages[0]);
             }}
           >
             {pages[0]}
@@ -77,11 +74,11 @@ export default function Pagination({
         <Link to={`/people?page=${pages[1]}`}>
           <li
             className={
-              currentPages === pages[1] ? 'page-item-selected' : 'page-item'
+              +currentPages === pages[1] ? 'page-item-selected' : 'page-item'
             }
             onClick={() => {
               onSelectNumber(pages[1]);
-              onRenderPageNumber(pages[1]);
+              setCurrentPages(pages[1]);
             }}
           >
             {pages[1]}
@@ -90,7 +87,7 @@ export default function Pagination({
         <li
           className="page-item"
           style={
-            currentPages > 3 && currentPages < pages.slice(-2)[0]
+            +currentPages > 3 && currentPages < pages.slice(-2)[0]
               ? { display: 'flex' }
               : { display: 'none' }
           }
@@ -113,7 +110,7 @@ export default function Pagination({
         <li
           className="page-item"
           style={
-            currentPages > 2 && currentPages < pages.slice(-3)[0]
+            +currentPages > 2 && currentPages < pages.slice(-3)[0]
               ? { display: 'block' }
               : { display: 'none' }
           }
@@ -123,13 +120,13 @@ export default function Pagination({
         <Link to={`/people?page=${pages.slice(-2)[0]}`}>
           <li
             className={
-              currentPages === pages.slice(-2)[0]
+              +currentPages === pages.slice(-2)[0]
                 ? 'page-item-selected'
                 : 'page-item'
             }
             onClick={() => {
               onSelectNumber(pages.slice(-2)[0]);
-              onRenderPageNumber(pages.slice(-2)[0]);
+              setCurrentPages(pages.slice(-2)[0]);
             }}
           >
             {pages.slice(-2)[0]}
@@ -138,13 +135,13 @@ export default function Pagination({
         <Link to={`/people?page=${pages.slice(-1)[0]}`}>
           <li
             className={
-              currentPages === pages.slice(-1)[0]
+              +currentPages === pages.slice(-1)[0]
                 ? 'page-item-selected'
                 : 'page-item'
             }
             onClick={() => {
               onSelectNumber(pages.slice(-1)[0]);
-              onRenderPageNumber(pages.slice(-1)[0]);
+              setCurrentPages(pages.slice(-1)[0]);
             }}
           >
             {pages.slice(-1)[0]}
@@ -152,23 +149,26 @@ export default function Pagination({
         </Link>
         <Link
           to={`/people?page=${
-            currentPages === pages.slice(-1)[0]
-              ? currentPages
-              : currentPages + 1
+            +currentPages === pages.slice(-1)[0] &&
+            +currentPages > pages.slice(-1)[0]
+              ? +currentPages
+              : +currentPages + 1
           }`}
         >
           <li
             className="page-item-select"
             onClick={() => {
               onSelectNumber(
-                currentPages === pages.slice(-1)[0]
+                +currentPages === pages.slice(-1)[0] &&
+                  +currentPages > pages.slice(-1)[0]
                   ? currentPages
-                  : currentPages + 1,
+                  : +currentPages + 1,
               );
-              onRenderPageNumber(
-                currentPages === pages.slice(-1)[0]
-                  ? currentPages
-                  : currentPages + 1,
+              setCurrentPages(
+                +currentPages === pages.slice(-1)[0] &&
+                  +currentPages > pages.slice(-1)[0]
+                  ? +currentPages
+                  : +currentPages + 1,
               );
             }}
           >
