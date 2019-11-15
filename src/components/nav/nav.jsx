c29365822d88fe63fe3dbac993d5c50e6fa2766f
alexmitse <alex1998mitsevich@gmail.com>
2019-11-15 14:04:05 +0300

    chore: fix bug

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -7,20 +7,29 @@ import './nav.css';
 const Nav = () => {
   return (
     <nav className=" navigation">
-      <Link to="/">
-        <h3 className="name-db">Star DB</h3>
-      </Link>
-      <ul className="nav-links">
-        <Link to="/people/:page">
-          <li>People</li>
+      <div className="img-star-wars">
+        <Link to="/">
+          <img
+            src="https://media.graytvinc.com/images/810*455/Star+Wars49.jpg"
+            alt="StarDB"
+            width="200px"
+            height="100px"
+          />
         </Link>
-        <Link to="/planet">
-          <li>Planets</li>
-        </Link>
-        <Link to="/starship">
-          <li>Starships</li>
-        </Link>
-      </ul>
+      </div>
+      <div className="container-nav-links">
+        <ul className="nav-links">
+          <Link to="/people/1">
+            <li>People</li>
+          </Link>
+          <Link to="/planet">
+            <li>Planets</li>
+          </Link>
+          <Link to="/starship">
+            <li>Starships</li>
+          </Link>
+        </ul>
+      </div>
     </nav>
   );
 };
