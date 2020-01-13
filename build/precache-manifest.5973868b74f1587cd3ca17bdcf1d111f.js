c389e3a8ad31ed16ba921af3930b186a79bc76ba
“alexmitse” <alex.mitsevich@gmail.com>
2020-01-13 17:06:26 +0300

    chore: add build for deploy in heroku

---
+++ build/precache-manifest.5973868b74f1587cd3ca17bdcf1d111f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef4507b1453a5b87b501a5a34c374d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59f4af26b63b5cbcfa55",
+    "url": "/static/css/main.9fe5ff62.chunk.css"
+  },
+  {
+    "revision": "302f24a8b7c6ca594a76",
+    "url": "/static/js/2.216c1b93.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/static/js/2.216c1b93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59f4af26b63b5cbcfa55",
+    "url": "/static/js/main.b1186bb8.chunk.js"
+  },
+  {
+    "revision": "98b6922fe3848941fac4",
+    "url": "/static/js/runtime-main.d58301f3.js"
+  }
+]);
